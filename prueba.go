6b560fe4473d6c7709932c9d32fb44510d09e4f2
Apocaliptus2012 <jorgeivan5456@gmail.com>
2023-02-28 18:30:25 -0500

    Primer algoritmo  teminado y probado

--- prueba.go
+++ prueba.go
@@ -12,17 +12,33 @@ func main() {
 
 	matriz3 := make([][]int, len(matriz1))
 
+	var a int = 1
+
 	for i := range matriz3 {
 		matriz3[i] = make([]int, len(matriz2[0]))
 	}
 
+	for i := 0; i < len(matriz1); i++ {
+		for j := 0; j < len(matriz2); j++ {
+			matriz1[i][j] = a
+		}
+
+	}
+
+	for i := 0; i < len(matriz2); i++ {
+		for j := 0; j < len(matriz2); j++ {
+			matriz2[i][j] = a
+		}
+	}
+
+	matriz3 = metodoNaivStandard(matriz1, matriz2, matriz3)
+
 	for i := 0; i < len(matriz3); i++ {
 		fmt.Println(matriz3[i])
 	}
-
 }
 
-func metodoNaivStandard(matriz1 [][]int, matriz2 [][]int, matriz3 [][]int) [][]int {
+func metodoNaivStandard(matriz1 [3][3]int, matriz2 [3][3]int, matriz3 [][]int) [][]int {
 
 	for i := 0; i < len(matriz1); i++ {
 		for j := 0; j < len(matriz2[0]); j++ {
