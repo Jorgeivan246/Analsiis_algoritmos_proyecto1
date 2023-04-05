430d904a881a00cf11159e40d88e2e2a67d626e1
Zaeboz <jegs2206@gmail.com>
2023-04-05 06:53:59 -0500

    ultima modificación

--- A15_V_3SequentialBlock.go
+++ A15_V_3SequentialBlock.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type A15_V_3SequentialBlock struct {
 }
 
@@ -17,7 +15,6 @@ func (s A15_V_3SequentialBlock) V_3SequentialBlock(A [][]int, B [][]int, C [][]i
 				for i := i1; i < i1+bsize && i < size; i++ {
 					for j := j1; j < j1+bsize && j < size; j++ {
 						for k := k1; k < k1+bsize && k < size; k++ {
-							fmt.Println("i: ", i, "j: ", j, "k: ", k)
 							C[k][i] += A[k][j] * B[j][i]
 						}
 					}
